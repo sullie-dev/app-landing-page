176f68fc8a8aa240174982ffa7d08d5602a2f530
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-05-23 23:13:31 +0100

    Admsted syntax for Navbar.js

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Link from "next";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div>
-      <Link href="#"></Link>
-      <Link href="#"></Link>
-      <Link href="#"></Link>
+      <Link href="#">
+        <a>Features</a>
+      </Link>
+      <Link href="#">
+        <a>Pricing</a>
+      </Link>
+      <Link href="#">
+        <a>About</a>
+      </Link>
     </div>
   );
 }

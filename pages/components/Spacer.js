e995c748c63bad62dd5dafea3d5c12e34de7e492
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-05-23 23:32:54 +0100

    added basic content and added Spacer.js - Spacer.js is a component to easily set a spaced box between html elements on pages

---
+++ pages/components/Spacer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Spacer(props) {
+  const { height } = props;
+  return <div style={{ height: height }}></div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Navbar from "./components/Navbar";
+import Spacer from "./components/Spacer";
 
 const index = () => {
   return (
     <div>
-        <Navbar/>
-      <h1>Hello there</h1>
+      <Navbar />
+      {/* TODO: add a space here to expand out the content  */}
+      <Spacer height="200px" />
+      <div>
+        <h1>Every putchase will be made with ease</h1>
+        <h6>
+          We work with gloabl card issuers to help ensure your payments go
+          through with out a hitch
+        </h6>
+      </div>
+      {/* TODO: add form for user to input their email for news letter */}
     </div>
   );
 };

5e98ad76e1a3f3229a78f68452d9e66314e3a43b
Eoghan O'Sullivan <eoghan@sullie.dev>
2021-05-25 11:49:40 +0100

    Added semantic ui, added input to form, added submit button to form

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "../styles/styles.css";
+import "/styles/styles.css";
+import "semantic-ui-css/semantic.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/components/EmailForm.js
+++ pages/components/EmailForm.js
@@ -1,15 +1,25 @@
-import styles from "/styles/EmailForm.module.css";
-
 import React from "react";
+import { Button, Icon,Form, Input } from "semantic-ui-react";
+import styles from "/styles/EmailForm.module.css";
 
 export default function EmailForm() {
   return (
     <div className={styles.container}>
       <div className={styles.emailInput}>
-        <h1>This is the email form</h1>
+        {/* <h1 className={styles.prompt}>Enter email address</h1> */}
+        <Form>
+    <Form.Field required>
+      <Input placeholder='Enter your email' />
+    </Form.Field>
+  </Form>
       </div>
       <div className={styles.submitButton}>
-        <button>Get Started</button>
+        <Button animated color='green'>
+          <Button.Content visible>Get Started</Button.Content>
+          <Button.Content hidden>
+            <Icon name="arrow right" />
+          </Button.Content>
+        </Button>
       </div>
     </div>
   );
